faf7b6d16eb1406eb3866adcf7680d3b52637d49
Garcez <allan.f.garcez@gmail.com>
2018-12-26 12:35:52 -0200

    Do a refactor in script

--- pastebineater.py
+++ pastebineater.py
@@ -1,48 +1,56 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import urllib
-import requests
-from bs4 import BeautifulSoup
-from urlparse import urlparse
-proxies = {
-    'https':'192.169.233.11:57260',
-    'http': '78.133.206.129:4145'
-        }
-#https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters
-fin = open('query','r')
-num_results=8
+import os
+from urllib.parse import urlparse
+try:
+    import requests
+    from bs4 import BeautifulSoup
+except:
+    os.system('python -m pip install -U requests --user')
+    os.system('python -m pip install -U beautifulsoup4 --user')
+    print('Agora você têm as dependências, cusão')
+    exit()
 
-for query in fin:
-  searc=query.strip('\n')
-  search=searc.replace(' ','+')
-  #URL="https://www.google.com/search?q={}&num={}".format(search,str(num_results))
-  # Time based search, previus 24h
-  URL="https://www.google.com/search?q={}&num={}&as_qdr=d".format(search,str(num_results))
-  # Previus seven days
-#  URL="https://www.google.com/search?q={}&num={}&as_qdr=w".format(search,str(num_results))
-  # Previus month
-#  URL="https://www.google.com/search?q={}&num={}&as_qdr=m".format(search,str(num_results))
-  #r=requests.get(URL,proxies=proxies)
-  r=requests.get(URL)
-  print "[+] URL: %s : STATUS CODE: %s" %( URL, r.status_code)
-  html=r.content
-  soup=BeautifulSoup(html,'html5lib')
-  links=soup.find_all('a')
-  filer=''
-  for link in links:
-    link_href=link.get('href')
-    if "url?q=" in link_href and not "webcache" in link_href:
-      grabber = link.get('href').split("?q=")[1].split("&sa=U")[0]
-      x = urlparse(grabber)
-      data = requests.get("https://pastebin.com/raw{0}".format(x[2])).text
-      encdata = data.encode('utf-8')
-      massdata = x[2].strip('/')
-      try:
-        fout = open(massdata,'a')
-        fout.write(data)
+
+def linkRestriction(link):
+    if "url?q=" in link and not "webcache" in link:
+        return True
+    return False
+
+
+def writeFile(name, text):
+# definir um diretório para escrita segura
+    try:
+        fout = open(name, 'a')
+        fout.write(text)
         fout.close()
-      except Exception as error:
-        continue
-      filer+='[+] URL> {}\n\n{}\n\n\n'.format(grabber, data)
-      print filer
-fin.close()
+        return True
+    except Exception:
+        return False
+
+
+PROXIES = {
+    'http': '78.133.206.129:4145',
+    'https': '192.169.233.11:57260'
+}
+queries = open('query', 'r')
+num_results = 8
+
+GOOGLE_QUERY = "https://www.google.com/search?q={}&num={}&as_qdr=d"
+PASTEBIN_RAW = "https://pastebin.com/raw{0}"
+
+for query in queries:
+    search = query.strip('\n').replace(' ', '+')
+    URL = GOOGLE_QUERY.format(search, str(num_results))
+    r = requests.get(URL)
+    print("[+] URL: %s : STATUS CODE: %s" % (URL, r.status_code))
+    if r.ok():
+        for link in BeautifulSoup(r.content, 'html.parser').find_all('a'):
+            href = link.get('href')
+            if linkRestriction(href):
+                grabber = href.split("?q=")[1].split("&sa=U")[0]
+                x = urlparse(grabber)
+                data = requests.get(PASTEBIN_RAW.format(x[2])).text
+                if writeFile("PasteBin_" + x[2].strip('/'), data):
+                    print('[+] URL> {}\n{}\n'.format(grabber, data))
+queries.close()
