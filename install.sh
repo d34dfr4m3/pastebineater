2c8292be330d2ae4193be0a00b38cb734d1392f7
Garcez <allan.f.garcez@gmail.com>
2018-12-26 12:35:42 -0200

    Install script in /usr/bin

---
+++ install.sh
@@ -0,0 +1,5 @@
+python -m pip install -U beautifulsoup4 --user
+python -m pip install -U requests --user
+
+echo 'Create a symlink in /usr/bin/pastebineater'
+sudo ln -s ./pastebineater /usr/bin/pastebineater
